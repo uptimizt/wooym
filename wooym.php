cf501547f9cf7dd9ae44dadb8c60a68d6e5e2e5f
yumashev <a.yumashev@gmail.com>
2017-11-01 20:30:58 +0300

    chg version

--- wooym.php
+++ wooym.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WooYM - WooCommerce и ЯДеньги
-Version: 0.7
+Version: 1.1
 Plugin URI: https://wpcraft.ru/product/wooym/
 Description: Интеграция WooCommerce и кошелька Яндекс Деньги (wooym)
 Author: WPCraft
@@ -10,10 +10,6 @@ Author URI: http://wpcraft.ru/
 
 require_once 'inc/class-wooym-getway.php';
 require_once 'inc/class-wooym-callback-endpoint.php';
-
-// require_once 'inc/class-email-sample.php';
-
-
 
 function wooym_woocommerce_add_gateway($methods) {
         $methods[] = 'WooYM_Getway';
